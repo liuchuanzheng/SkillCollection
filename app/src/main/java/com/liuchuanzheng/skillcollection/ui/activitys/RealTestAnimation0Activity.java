2c89d0e662eb4a528dabcc7ae4760ac2459478f8
liuchuanzheng <liuchuanzheng@feiliu.com>
2017-07-07 15:11:58 +0800

    完成view 动画   (tween补间动画)

--- app/src/main/java/com/liuchuanzheng/skillcollection/ui/activitys/RealTestAnimation0Activity.java
+++ app/src/main/java/com/liuchuanzheng/skillcollection/ui/activitys/RealTestAnimation0Activity.java
@@ -4,9 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.liuchuanzheng.skillcollection.R;
 import com.liuchuanzheng.skillcollection.base.LCZBaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by 刘传政 on 2017/7/4 0004.
@@ -17,17 +26,32 @@ import com.liuchuanzheng.skillcollection.base.LCZBaseActivity;
  */
 
 public class RealTestAnimation0Activity extends LCZBaseActivity {
+    @BindView(R.id.iv)
+    ImageView iv;
+    @BindView(R.id.set)
+    Button set;
+    @BindView(R.id.alpha)
+    Button alpha;
+    @BindView(R.id.scale)
+    Button scale;
+    @BindView(R.id.translate)
+    Button translate;
+    @BindView(R.id.rotate)
+    Button rotate;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_animation_0);
+        ButterKnife.bind(this);
         realTest();
     }
 
     /**
      *
      */
-    private void realTest() {}
+    private void realTest() {
+    }
 
     /**
      * 入口
@@ -39,5 +63,29 @@ public class RealTestAnimation0Activity extends LCZBaseActivity {
         activity.startActivity(intent);
         activity.overridePendingTransition(R.anim.fade_in,
                 R.anim.fade_out);
+    }
+
+    @OnClick({R.id.set, R.id.alpha, R.id.scale, R.id.translate, R.id.rotate})
+    public void onViewClicked(View view) {
+        Animation animation = null;
+        switch (view.getId()) {
+            case R.id.set:
+                animation = AnimationUtils.loadAnimation(baseContext,R.anim.set);
+                break;
+            case R.id.alpha:
+                animation = AnimationUtils.loadAnimation(baseContext,R.anim.alpha);
+                break;
+            case R.id.scale:
+                animation = AnimationUtils.loadAnimation(baseContext,R.anim.scale);
+                break;
+            case R.id.translate:
+                animation = AnimationUtils.loadAnimation(baseContext,R.anim.translate);
+                break;
+            case R.id.rotate:
+                animation = AnimationUtils.loadAnimation(baseContext,R.anim.rotate);
+                break;
+
+        }
+        iv.startAnimation(animation);
     }
 }
