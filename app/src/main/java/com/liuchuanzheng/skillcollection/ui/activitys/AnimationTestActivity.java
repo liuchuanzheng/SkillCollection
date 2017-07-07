7a0439b0901a22ad3cbb35a03323dea40c56d41c
liuchuanzheng <liuchuanzheng@feiliu.com>
2017-07-07 11:51:03 +0800

    动画recyclerView布局完成

--- app/src/main/java/com/liuchuanzheng/skillcollection/ui/activitys/AnimationTestActivity.java
+++ app/src/main/java/com/liuchuanzheng/skillcollection/ui/activitys/AnimationTestActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,7 +35,9 @@ public class AnimationTestActivity extends LCZBaseActivity {
     ImageButton ibRight;
     @BindView(R.id.tv_message)
     TextView tvMessage;
-
+    @BindView(R.id.rcv)
+    RecyclerView rcv;
+    private MyAdapter mAdapter;//recyclerview的适配器
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +50,19 @@ public class AnimationTestActivity extends LCZBaseActivity {
     private void initView() {
         tvTitle.setText("动画");
         tvMessage.setText("对原生的Android动画进行测试");
+        rcv.setLayoutManager(new GridLayoutManager(this, 1));
+        mAdapter = new MyAdapter();
+        rcv.setAdapter(mAdapter);
+        mAdapter.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(int position) {
+            }
+
+            @Override
+            public void onLongClick(int position) {
+
+            }
+        });
     }
 
     /**
@@ -59,6 +79,78 @@ public class AnimationTestActivity extends LCZBaseActivity {
 
     @OnClick(R.id.ib_right)
     public void onViewClicked() {
-        Toast.makeText(baseContext,"请对下方具体条目点击",Toast.LENGTH_SHORT).show();
+        Toast.makeText(baseContext, "请对下方具体条目点击", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 为recyclerView定义的点击接口
+     */
+    public interface OnClickListener {
+        void onClick(int position);
+
+        void onLongClick(int position);
+    }
+
+    /**
+     * recyclerView的adapter
+     */
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        private OnClickListener onClickListener = null;
+
+        public void setOnClickListener(OnClickListener onClickListener) {
+            this.onClickListener = onClickListener;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    baseContext).inflate(R.layout.item_animation, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(final MyViewHolder holder, int position) {
+            if(0 == position){
+                holder.tv.setText("View Animation tween 补间动画");
+            }else if (1 == position){
+
+            }else if (2 == position){
+
+            }else if (3 == position){
+
+            }
+            if (onClickListener != null) {
+                holder.tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onClickListener.onClick(holder.getAdapterPosition());
+                    }
+                });
+                holder.tv.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        onClickListener.onLongClick(holder.getAdapterPosition());
+                        return false;
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 4;
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView tv;
+
+            public MyViewHolder(View view) {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.tv_item);
+            }
+        }
     }
 }
